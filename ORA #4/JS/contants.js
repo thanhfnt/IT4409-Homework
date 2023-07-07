0f8c4292500292f3034fdc95f9760d367310bf28
Cao Van The Anh <52328451+Makacha@users.noreply.github.com>
2023-07-07 10:17:43 +0700

    Update contants.js

--- ORA #4/JS/contants.js
+++ ORA #4/JS/contants.js
@@ -1,4 +1,4 @@
-const CLIENT_ID = '750807168690-r2qarv4c0d7ceht2e00245i3ojbunfmi.apps.googleusercontent.com';
-const API_KEY = 'AIzaSyAtMzsdBqxZ7cHaHTYNUz0P7N4YKlZZpYU';
+const CLIENT_ID = 'your-client-id';
+const API_KEY = 'your-api-key';
 const DISCOVERY_DOC = 'https://www.googleapis.com/discovery/v1/apis/gmail/v1/rest';
 const SCOPES = 'https://mail.google.com/';
